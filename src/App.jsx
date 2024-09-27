dbef4e83d14be8e53940e16ef44ab10ad289b739
developer-abdulali <ab.ali.soomro@gmail.com>
2024-09-28 02:24:27 +0500

    added changes maked ui responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import Footer from "./components/Footer/Footer";
 import Manager from "./components/Manager/Manager";
 import Navbar from "./components/Navbar/Navbar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
+      <ToastContainer />
       <Navbar />
-      <Manager />
+      <div className="min-h-[83vh]">
+        <Manager />
+      </div>
+      <Footer />
     </>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 const Navbar = () => {
   return (
-    <div className="bg-slate-800 text-white">
-      <div className="mycontainer flex items-center justify-between py-5 h-14">
+    <div className="bg-slate-800 text-white sticky top-0">
+      <div className="px-2 md:px-0 mycontainer flex items-center justify-between py-5 h-14 z-50">
         <div className="logo font-bold text-white text-2xl">
           <span className="text-green-500"> &lt;</span>
           Pass<span className="text-green-500">OP/&gt;</span>
         </div>
 
-        <ul>
+        {/* <ul>
           <li className="flex gap-4">
             <a href="/" className="hover:font-bold">
               Home
@@ -19,7 +19,21 @@ const Navbar = () => {
               Contact
             </a>
           </li>
-        </ul>
+        </ul> */}
+
+        <button
+          onClick={() =>
+            window.open("https://github.com/developer-abdulali", "_blank")
+          }
+          className="text-white bg-green-700 my-5 mx-2 rounded-full flex items-center justify-between ring-1 ring-white"
+        >
+          <img
+            src="/icons/github.svg"
+            alt="Github logo"
+            className="invert w-10 p-1"
+          />
+          <span className="font-semibold px-2">Github</span>
+        </button>
       </div>
     </div>
   );
