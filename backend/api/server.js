55754739a1436b30903dfd2b184e951eedfc02b7
developer-abdulali <ab.ali.soomro@gmail.com>
2024-10-08 01:36:51 +0500

    added changes

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import connectDB from "./config/db.js";
+import connectDB from "../config/db.js";
 import dotenv from "dotenv";
-import connectCloudinary from "./config/cloudinary.js";
-import adminRouter from "./routes/adminRoute.js";
-import doctorRouter from "./routes/doctorRoute.js";
-import userRouter from "./routes/userRoute.js";
+import connectCloudinary from "../config/cloudinary.js";
+import adminRouter from "../routes/adminRoute.js";
+import doctorRouter from "../routes/doctorRoute.js";
+import userRouter from "../routes/userRoute.js";
 dotenv.config();
 
 // app config
