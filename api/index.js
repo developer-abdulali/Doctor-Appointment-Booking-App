653e42606c805cb55342f3e6dfbaac38773e2965
developer-abdulali <ab.ali.soomro@gmail.com>
2025-01-01 12:53:49 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,41 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import connectDB from "./config/db.js";
-// import connectCloudinary from "./config/cloudinary.js";
-// import adminRouter from "./routes/adminRoute.js";
-// import doctorRouter from "./routes/doctorRoute.js";
-// import userRouter from "./routes/userRoute.js";
-
-// dotenv.config();
-
-// // App configuration
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// // Connect to database and Cloudinary
-// connectDB();
-// connectCloudinary();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // API endpoints
-// app.use("/api/admin", adminRouter);
-// app.use("/api/doctor", doctorRouter);
-// app.use("/api/user", userRouter);
-
-// // Test endpoint
-// app.get("/", (req, res) => {
-//   res.send("Doctor Booking Server is working");
-// });
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -47,16 +9,13 @@ import userRouter from "./routes/userRoute.js";
 
 dotenv.config();
 
-// App configuration
 const app = express();
 
-// Connect to database and Cloudinary
 connectDB();
 connectCloudinary();
 
 // Middleware
 app.use(express.json());
-// app.use(cors());
 app.use(
   cors({
     origin: [
